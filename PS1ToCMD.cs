d25bb8fa2679a1ab8d60437ced146e48009d81bb
Kirill Illenseer <Kirill.Illenseer@landisgyr.com>
2022-04-21 09:58:24 +0200

    Add project files.

---
+++ PS1ToCMD.cs
@@ -0,0 +1,3 @@
+﻿var InFile = Environment.GetCommandLineArgs()[1];
+var OutFile = Path.ChangeExtension(InFile, "cmd");
+File.WriteAllText(OutFile, "@PowerShell -ExecutionPolicy RemoteSigned -EncodedCommand " + Convert.ToBase64String(System.Text.Encoding.Unicode.GetBytes(File.ReadAllText(InFile))));
